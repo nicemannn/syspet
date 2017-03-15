fad684ccd9d2c4199efeaca016c68b958e3dc40c
mac <mac@macs-air>
2017-03-14 22:13:55 -0300

    Exemplo de como deve ficar as classes modelos, encapsulamento e gets e sets.

--- Syspet_3_0/src/modelo/Animais.java
+++ Syspet_3_0/src/modelo/Animais.java
@@ -10,10 +10,61 @@ package modelo;
  * @author Bebe Iucas
  */
 public class Animais {
-    String nome;
-    String tipo;
-    String raca;
-    double peso;
-    double tamanho;
-    int idade;
+    
+    private String nome;
+    private String tipo;
+    private String raca;
+    private double peso;
+    private double tamanho;
+    private int idade;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    public String getRaca() {
+        return raca;
+    }
+
+    public void setRaca(String raca) {
+        this.raca = raca;
+    }
+
+    public double getPeso() {
+        return peso;
+    }
+
+    public void setPeso(double peso) {
+        this.peso = peso;
+    }
+
+    public double getTamanho() {
+        return tamanho;
+    }
+
+    public void setTamanho(double tamanho) {
+        this.tamanho = tamanho;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+    
+    
 }
